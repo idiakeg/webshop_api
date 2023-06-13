182f47762db50dfa35df7a513298d3aa4e4b433b
Godspower Idiakose <idiakeg@gmail.com>
2023-06-13 21:32:31 +0100

    Included the isRevoked property
    	- This property makes it such that even if an user is
    	  authenticated and does not have certain privileges, some end
    	  points will not be accessbile to them.
    
    	- An example is dor users that are just customers, when they are
    	  authenticated, they can view the product, but to be a able to
    	  create or delete a product, u have to be an admin.
    
    	- The payload in the helper function isRevoked houses whatever
    	  information was specified in the token, hence the need to
    	  include the isAdmin information in the token.

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -8,13 +8,23 @@ const authJwt = () => {
 	return expressJwt({
 		secret,
 		algorithms: ["HS256"],
+		isRevoked: isRevoked,
 	}).unless({
 		path: [
-			{ url: /\/api\/v1\/products(.*)/, methods: ["GET"] },
+			{ url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
 			{ url: /\/api\/v1\/categories(.*)/, methods: ["GET"] },
 			`${api}/users/login`,
 		],
 	});
 };
+
+// payload contains data present in the token
+
+async function isRevoked(req, payload) {
+	if (payload.isAdmin === false) {
+		return true;
+	}
+	return false;
+}
 
 module.exports = authJwt;
