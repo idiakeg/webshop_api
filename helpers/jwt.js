96d018ae202b33e162244e2845630a6e8e5ab499
Godspower Idiakose <idiakeg@gmail.com>
2023-06-13 11:50:53 +0100

    Included the unless method
    	- This method enables me specify what routes/end points
    	  unauthorized users can access

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -4,9 +4,16 @@ const { expressjwt: expressJwt } = require("express-jwt");
 
 const authJwt = () => {
 	const secret = process.env.SECRET;
+	const api = process.env.API_ROOT;
 	return expressJwt({
 		secret,
 		algorithms: ["HS256"],
+	}).unless({
+		path: [
+			{ url: /\/api\/v1\/products(.*)/, methods: ["GET"] },
+			{ url: /\/api\/v1\/categories(.*)/, methods: ["GET"] },
+			`${api}/users/login`,
+		],
 	});
 };
 
