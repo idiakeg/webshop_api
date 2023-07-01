091f49998e18eafe246c25075d5fe50cabb37569
Godspower Idiakose <idiakeg@gmail.com>
2023-07-01 09:56:52 +0100

    created the order model

--- models/order_model.js
+++ models/order_model.js
@@ -1,6 +1,61 @@
 const { Schema, model } = require("mongoose");
 
-const orderSchema = new Schema({});
+const orderSchema = new Schema({
+	OrderItems: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "OrderItems",
+			required: true,
+		},
+	],
+	shippingAddress1: {
+		type: String,
+		required: true,
+	},
+	shippingAddress2: {
+		type: String,
+	},
+	city: {
+		type: String,
+		required: true,
+	},
+	zip: {
+		type: String,
+		required: true,
+	},
+	country: {
+		type: String,
+		required: true,
+	},
+	phone: {
+		type: String,
+		required: true,
+	},
+	status: {
+		type: String,
+		required: true,
+		default: "Pending",
+	},
+	totalPrice: {
+		type: Number,
+	},
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+	},
+	dateCreated: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+orderSchema.virtual("id").get(function () {
+	return this._id.toHexString();
+});
+
+orderSchema.set("toJSON", {
+	virtuals: true,
+});
 
 const OrderModel = model("Order", orderSchema);
 
